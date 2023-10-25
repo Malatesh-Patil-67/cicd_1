18831739bf96f289aa7b15c0cc1a4d9b9e55f0a4
malatesh <malateshpatil6767@gmail.com>
2023-10-25 11:45:09 +0200

    new

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
-"""
-This is a simple Flask web application.
-
-It provides a basic web service that responds with a 'Hello, World!' message when accessed.
-"""
-
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """
+    This function handles the root route.
+
+    It returns a 'Hello, World!' message when the root route is accessed.
+
+    Returns:
+        str: A greeting message.
+    """
     return 'Hello, World!'
 
 if __name__ == "__main__":
