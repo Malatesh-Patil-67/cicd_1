2f17c34592537a3fd415f1247214b3317a650542
malatesh <malateshpatil6767@gmail.com>
2023-10-25 11:41:15 +0200

    hi

--- app.py
+++ app.py
@@ -4,10 +4,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    """
+    This function handles the root endpoint and returns a greeting.
 
+    Returns:
+        str: A greeting message.
+    """
+    return 'Hello, World!'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
-
-
