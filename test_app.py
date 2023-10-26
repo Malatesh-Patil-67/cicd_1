e7b87e5dbc1d79a379f48caa6e7daeba3de6a198
malatesh <malateshpatil6767@gmail.com>
2023-10-26 17:07:41 +0200

    hi

--- test_app.py
+++ test_app.py
@@ -1,22 +1,22 @@
 import pytest
 from app import app
 
-
 @pytest.fixture
 def client():
     app.config['TESTING'] = True
     client = app.test_client()
     yield client
 
-
+def test_home_page(client):
+    response = client.get('/')
+   
+    assert b"Enter Stock Symbol:" in response.data
 
 def test_stock_data_submission(client):
     response = client.post('/', data={'stock_symbol': 'AAPL'})
-    assert b'AAPL' in response.data
+    assert b"AAPL" in response.data
 
 def test_stock_data_plot(client):
     response = client.post('/', data={'stock_symbol': 'AAPL'})
-    assert b'Price' in response.data  # Check for the 'Price' label on the plot image.
-
-def test_example():
-    assert 1 == 1
+    assert b"Stock Symbol:" in response.data
+ # Adjust this text according to your "home.html" template
